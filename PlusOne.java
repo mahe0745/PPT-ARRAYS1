0f7c6315be3041ef72fc287e510ff1801e6d9b25
mahe0745 <106669437+mahe0745@users.noreply.github.com>
2023-05-31 22:49:31 +0530

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -1,5 +1,16 @@
 package Assignment1;
 
 public class PlusOne {
+  for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
 
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
+}
 }
